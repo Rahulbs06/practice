b4438a39cdb4a6b39ecc9c54b37bc45298bf3503
Rahulbs06 <rahulbs697@gmail.com>
2025-01-01 15:56:50 +0530

    second commit

--- dbutilites.java
+++ dbutilites.java
@@ -1,2 +1,2 @@
  Hi this is rahul practicing git commands
-
+this file is also moved from staging to local area
--- walmart.java
+++ walmart.java
@@ -1,1 +1,2 @@
 this is my second file in git local repo
+now i moved this file from staging to local area
